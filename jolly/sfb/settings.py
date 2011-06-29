defdd0db4cdf3fd107355b1e93e2299f973e536e
robert <robert@ugly.(none)>
2011-06-29 17:47:38 -0400

    changed SOP path

--- jolly/sfb/settings.py
+++ jolly/sfb/settings.py
@@ -1,1 +1,1 @@
-SEQUENCE_OF_PLAY = 'C:/Documents And Settings/rcathro-oliver/My Documents/jolly/sfb/config/sequence-of-play.xml'
+SEQUENCE_OF_PLAY = '/home/robert/projects/jolly/src/jolly/sfb/config/sequence-of-play.xml'
